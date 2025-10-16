0cc9caa1e6c7fa572246defb25bc427ea4a2a110
Maddox <mgreenspoon19@gmail.com>
2025-10-16 08:43:03 -0400

    Traffic

--- Essentials/Assets/_Unity Essentials/Scripts/CarSpawner.cs
+++ Essentials/Assets/_Unity Essentials/Scripts/CarSpawner.cs
@@ -5,10 +5,13 @@ public class CarSpawner : MonoBehaviour
     public GameObject carPrefab;
     public Transform startPoint;
     public Transform endPoint;
+     public float spawnInterval = 3f; // Time (in seconds) between spawns
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        SpawnCar();
+        // Start a repeating loop that spawns cars
+        InvokeRepeating(nameof(SpawnCar), 0f, spawnInterval);
     }
 
     
